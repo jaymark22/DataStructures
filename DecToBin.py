b15b5a9178713e01fd2f3d95fd6b32d63c96d078
Eadrian Basila <basilaeadrian@gmail.com>
2020-10-22 02:57:39 +0800

    Activity2 [Unfinished]

---
+++ DecToBin.py
@@ -0,0 +1,13 @@
+#Using Recursive Algorithm
+def DecToBin(num):
+    list = []
+    if num > 1: 
+        DecToBin(num // 2) 
+        list.append(num % 2)
+        print(list)
+     
+if __name__ == '__main__': 
+      
+    numVal=int(input("Enter a decimal number: "))
+    DecToBin(numVal) 
+   
